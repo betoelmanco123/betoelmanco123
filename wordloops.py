a68a3267ca4eee11764af79e33f648334e78cd7a
betoelmanco123 <162057572+betoelmanco123@users.noreply.github.com>
2025-05-01 12:17:25 -0600

    Update wordloops.py

--- wordloops.py
+++ wordloops.py
@@ -3,11 +3,6 @@ elements = [
     # Letras minúsculas
     ' ','a','b','c','d','e','f','g','h','i','j','k','l','m',
     'n','o','p','q','r','s','t','u','v','w','x','y','z',
-
-    # Letras mayúsculas
-    'A','B','C','D','E','F','G','H','I','J','K','L','M',
-    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-
     # Números
     '0','1','2','3','4','5','6','7','8','9',
 
@@ -30,14 +25,13 @@ for i in wordList:
         new = ''.join(ready)
         if ready:
             try:
-                if sys.argv[1]:
-                    value = (f'\r{new}{k}',end='')
+                if sys.argv[1]  == '-r':
+                    print( f'\r{new}{k}',end='')
+                else:
+                    print(f'{new}{k}')    
             except:
-             
-            else:
-                value = f'{new}{k}'
-            print(value)
+                print(f'{new}{k}')
             time.sleep(0.06)
-        if k == i:
+        if k == i.lower():
             ready.append(k)
             break
